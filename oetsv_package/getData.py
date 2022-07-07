c51377c083763a23f38690cbab80a931dfce03f3
Coeptus <jkbseibert@gmail.com>
2022-07-08 01:18:04 +0200

    Adressen und Stargeld wird richtig angezeigt Keine Auslöndischen Adressen werden angezeigt Startgeld wird allgemeiner gesucht

--- oetsv_package/getData.py
+++ oetsv_package/getData.py
@@ -24,11 +24,11 @@ def getData(id):
         datum=df.get('nr').where(df.get('nr').str.find('DATUM')==0).dropna().to_string().split('=')[1]
         zeit=re.sub(' Uhr','',re.search("\d\d?(|:\d\d) Uhr",soup.text).group(0))+":00"
 
-        nenngeld_suche=re.search("(g|G)(eld|gebühr)\s*\d+(,(\d{2}|-))\s*€?",soup.text)
+        nenngeld_suche=re.search("Nenngeld:(\s|\w|€)*\d+(,(\d{2}|-)|)",soup.text)
         if nenngeld_suche:
-            nenngeld=re.sub('\s+','',re.sub('(G|g)(eld|gebühr) ','',nenngeld_suche.group(0)))
+            nenngeld=re.search('\d+(,(\d{2}|-)|)',nenngeld_suche.group(0)).group(0)
         else:
-            nenngeld=0
+            nenngeld='0'
         #turnierklassen
         table=soup.find('table').find_all('tr')[3].find_all('td')
         klassen_html=table[len(table)-1]
@@ -47,11 +47,14 @@ def getData(id):
             links.append(i['href'])
 
         link_str='-'.join([str(elem) for elem in links])
-        search_term=re.sub('q=','',re.sub('\s+|\+','%20',re.search("q=(\w|\s|\+)+",link_str).group(0)))
+        search_term=re.sub('q=','',re.sub('\s+|\+','%20',re.search("q=(\w|\s|\+|\.)+",link_str).group(0)))
         adr_response=json.loads(requests.get("https://nominatim.openstreetmap.org/search?q="+search_term+"&format=json").text)
-        if len(adr_response)!=0:
-            adr=adr_response[0]['display_name']
-        else:
+        adr=""
+        for a in adr_response:
+            if re.search('Österreich',a['display_name']):
+                adr=a['display_name']
+                break
+        if adr=="":
             adr=re.sub('%20',' ',search_term)
 
         #in Dict für JSON
