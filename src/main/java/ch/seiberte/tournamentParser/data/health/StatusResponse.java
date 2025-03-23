280999e9304828267338fadd787b58fd0cc1d283
Coeptus <jaksei.lol@gmail.com>
2025-03-23 22:55:18 +0100

    Changed StatusResponse to Record

--- src/main/java/ch/seiberte/tournamentParser/data/health/StatusResponse.java
+++ src/main/java/ch/seiberte/tournamentParser/data/health/StatusResponse.java
@@ -1,19 +1,4 @@
 package ch.seiberte.tournamentParser.data.health;
 
-public class StatusResponse {
-    private Status status;
-    private String message;
-
-    public StatusResponse(Status status, String message) {
-        this.status = status;
-        this.message = message;
-    }
-
-    public Status getStatus() {
-        return status;
-    }
-
-    public String getMessage() {
-        return message;
-    }
+public record StatusResponse(Status status, String message) {
 }
