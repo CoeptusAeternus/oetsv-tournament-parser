3ee50111e2ea08095b9d6ace80dfe449adef5e81
Jakob Seibert <jakob@seibert.wien>
2026-05-10 18:39:55 +0000

    Refactor SiteErrorController to remove ErrorController implementation

--- src/main/java/ch/seiberte/tournamentParser/SiteErrorController.java
+++ src/main/java/ch/seiberte/tournamentParser/SiteErrorController.java
@@ -1,9 +1,7 @@
 package ch.seiberte.tournamentParser;
 
-
-import org.springframework.boot.web.servlet.error.ErrorController;
-
 import org.springframework.web.bind.annotation.ControllerAdvice;
 
 @ControllerAdvice
-public class SiteErrorController implements ErrorController {}
+public class SiteErrorController {
+}
